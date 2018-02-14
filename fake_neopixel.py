5a1654efe35779fd7fa6e2335b7c7eb6eff23e91
JJTheo <theo.jeanjean42@gmail.com>
2018-02-14 17:38:04 +0100

    fake ok

---
+++ fake_neopixel.py
@@ -0,0 +1,34 @@
+class Adafruit_NeoPixel(object):
+    def __init__(self, num, pin, freq_hz=800000, dma=5, invert=False,
+                 brightness=255, channel=0, strip_type=0):
+        pass
+
+    def _cleanup(self):
+        pass
+
+    def begin(self):
+        pass
+
+    def show(self):
+        pass
+
+    def setPixelColor(self, n, color):
+        pass
+
+    def setPixelColorRGB(self, n, red, green, blue, white=0):
+        pass
+
+    def setBrightness(self, brightness):
+        pass
+
+    def getBrightness(self):
+        pass
+
+    def getPixels(self):
+        pass
+
+    def numPixels(self):
+        pass
+
+    def getPixelColor(self, n):
+        pass
