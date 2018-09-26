4aeb04b0730e4ccd56c8fb12d13e6c78c5a61e26
Etienne Barbier <barbier.dev@gmail.com>
2018-09-26 19:42:53 +0200

    Change api path

--- server.py
+++ server.py
@@ -1,8 +1,10 @@
 import conf
 import threading
 import logging
-from flask import Flask, request, redirect, render_template, jsonify
+from flask import Flask, request, redirect, render_template, jsonify, Response
 from werkzeug.serving import make_server
+from flask_cors import CORS
+
 
 
 def set_debug():
@@ -16,6 +18,22 @@ def apply_change(self):
         conf.start = False
         self.event_end_start.set()
 
+def start_led(self):
+    if not conf.start:
+        conf.start = True
+        self.event_end_start.set()
+
+def restart_led(self):
+    if conf.start:
+        conf.restart = True
+        conf.start = False
+        self.event_end_start.set()
+
+def stop_led(self):
+    if conf.start:
+        conf.start = False
+        self.event_end_start.set()
+
 
 class Server(threading.Thread):
 
@@ -25,6 +43,7 @@ class Server(threading.Thread):
         threading.Thread.__init__(self)
         global app
         app = Flask(__name__)
+        CORS(app)
         self.srv = make_server('127.0.0.1', 5000, app)
 
     def run(self):
@@ -32,70 +51,64 @@ class Server(threading.Thread):
         def default_page():
             return render_template('index.html')
 
-        @app.route('/start')
-        def start():
-            if not conf.start:
-                conf.start = True
-                self.event_end_start.set()
-            return 'start ok',200
+        @app.route('/action', methods=['GET'])
+        def action():
+            if request.args.get('power') != None:
+                arg_power = request.args.get('power')
+                if arg_power == 'start':
+                    start_led(self)
+                elif arg_power == 'restart':
+                    restart_led(self)
+                elif arg_power == 'stop':
+                    stop_led(self)
+                else:
+                    return Response(response=None,status=404,mimetype="application/json")
+                return Response(response=None,status=200,mimetype="application/json")
+            else:
+                return Response(response=None,status=404,mimetype="application/json")
 
-        @app.route('/restart')
-        def restart():
-            if conf.start:
-                conf.restart = True
-                conf.start = False
-                self.event_end_start.set()
-            return 'restart ok',200
-
-        @app.route('/stop')
-        def stop():
-            if conf.start:
-                conf.start = False
-                self.event_end_start.set()
-            return 'stop ok',200
 
         @app.route('/state')
         def state():
             state = "stopped"
             if conf.start:
                 state = "started"
-            return jsonify({'state': state, 'program': conf.current}),200
+            return Response(response={'state': state, 'program': conf.current},status=200,mimetype="application/json")
 
-        @app.route('/setTheaterChaseRainbow')
-        def setTheaterChaseRainbow():
-            conf.current = "THEATER_CHASE_RAINBOW"
-            apply_change(self)
-            return 'setRainbow ok',200
 
-        @app.route('/setRainbow')
-        def setRainbow():
-            conf.current = "RAINBOW"
-            apply_change(self)
-            return 'setRainbow ok',200
+        @app.route('/annimation', methods=['GET'])
+        def annimation():
+            if request.args.get('id') != None:
+                arg_annimation = request.args.get('id')
+                if arg_annimation == 'theater_chase_rainbow':
+                    conf.current = "THEATER_CHASE_RAINBOW"
+                    apply_change(self)
+                elif arg_annimation == 'rainbow':
+                    conf.current = "RAINBOW"
+                    apply_change(self)
+                elif arg_annimation == 'rainbow_cycle':
+                    conf.current = "RAINBOW_CYCLE"
+                    apply_change(self)
+                elif arg_annimation == 'all':
+                    conf.current = "ALL"
+                    apply_change(self)
+                else:
+                    return Response(response=None,status=404,mimetype="application/json")
+                return Response(response=None,status=200,mimetype="application/json")
+            else:
+                return Response(response=None,status=404,mimetype="application/json")
 
-        @app.route('/setRainbowCycle')
-        def setRainbowCycle():
-            conf.current = "RAINBOW_CYCLE"
-            apply_change(self)
-            return 'setRainbowCycle ok',200
-
-        @app.route('/setAll')
-        def setAll():
-            conf.current = "ALL"
-            apply_change(self)
-            return 'setAll ok',200
-
-        @app.route('/setFixedColor', methods=['GET'])
+        @app.route('/color', methods=['GET'])
         def setFixedColor():
-            if request.args.get('r') != None:
-                conf.color[0] = int(request.args.get('r'))
-            if request.args.get('g') != None:
-                conf.color[1] = int(request.args.get('g'))
-            if request.args.get('b') != None:
-                conf.color[2] = int(request.args.get('b'))
+            if request.args.get('red') != None:
+                conf.color[0] = int(request.args.get('red'))
+            if request.args.get('green') != None:
+                conf.color[1] = int(request.args.get('green'))
+            if request.args.get('blue') != None:
+                conf.color[2] = int(request.args.get('blue'))
             conf.current = "FIXED_COLOR"
             apply_change(self)
-            return 'setFixedColor ok',200
+            return Response(response=None,status=200,mimetype="application/json")
 
 
         print("#### Start Web Server ####")
